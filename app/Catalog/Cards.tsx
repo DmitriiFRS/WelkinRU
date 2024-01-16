52755558e6bd1bc59165b04a3e9eb6eb3b0dd5aa
Дмитрий <Dmitriifrs@gmail.com>
2024-01-16 16:07:45 +0500

    fix

--- app/Catalog/Cards.tsx
+++ app/Catalog/Cards.tsx
@@ -1,70 +1,6 @@
 import styles from "./catalog.module.scss";
-import example from "../../public/img/catalog/example.png";
-import SortBlock from "./SortBlock";
 import Content from "./Items/Content";
 
-const items = [
-   {
-      id: 0,
-      img: example,
-      name: "Welkin H30/5R1B",
-      type: "Модульный чиллер",
-   },
-   {
-      id: 1,
-      img: example,
-      name: "Welkin H30/5R1B",
-      type: "Модульный чиллер",
-   },
-   {
-      id: 2,
-      img: example,
-      name: "Welkin H30/5R1B",
-      type: "Модульный чиллер",
-   },
-   {
-      id: 3,
-      img: example,
-      name: "Welkin H30/5R1B",
-      type: "Модульный чиллер",
-   },
-   {
-      id: 4,
-      img: example,
-      name: "Welkin H30/5R1B",
-      type: "Модульный чиллер",
-   },
-   {
-      id: 5,
-      img: example,
-      name: "Welkin H30/5R1B",
-      type: "Модульный чиллер",
-   },
-   {
-      id: 6,
-      img: example,
-      name: "Welkin H30/5R1B",
-      type: "Модульный чиллер",
-   },
-   {
-      id: 7,
-      img: example,
-      name: "Welkin H30/5R1B",
-      type: "Модульный чиллер",
-   },
-   {
-      id: 8,
-      img: example,
-      name: "Welkin H30/5R1B",
-      type: "Модульный чиллер",
-   },
-   {
-      id: 9,
-      img: example,
-      name: "Welkin H30/5R1B",
-      type: "Модульный чиллер",
-   },
-];
 type ItemType = {
    attributes: {
       name: string;
@@ -76,7 +12,7 @@ type ItemType = {
 
 type Props = Array<ItemType>;
 
-function Cards({ items }: { items: Props }) {
+function Cards({ items }: { items: any }) {
    return (
       <section className={styles.cards}>
          <Content items={items} />
