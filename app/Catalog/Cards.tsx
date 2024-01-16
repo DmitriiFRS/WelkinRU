6d3d4febcab4079bd40c1ad25aa34e84971fec2f
Дмитрий <Dmitriifrs@gmail.com>
2024-01-16 16:09:34 +0500

    fix

--- app/Catalog/Cards.tsx
+++ app/Catalog/Cards.tsx
@@ -1,17 +1,6 @@
 import styles from "./catalog.module.scss";
 import Content from "./Items/Content";
 
-type ItemType = {
-   attributes: {
-      name: string;
-      type: string;
-      reference: string;
-      image: any;
-   };
-};
-
-type Props = Array<ItemType>;
-
 function Cards({ items }: { items: any }) {
    return (
       <section className={styles.cards}>
