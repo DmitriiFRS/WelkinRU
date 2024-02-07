17b1a4966626d2618991af42aee4e5eb5de5f1ba
Дмитрий <Dmitriifrs@gmail.com>
2024-02-07 14:15:31 +0500

    fix

--- app/faq/Col.tsx
+++ app/faq/Col.tsx
@@ -3,7 +3,7 @@ import styles from "./faq.module.scss";
 
 function Col({ el, id }: { el: string; id: number }) {
    return (
-      <Link href={"faq" + id.toString()} className={styles.faq__item}>
+      <Link href={"faq/" + id.toString()} className={styles.faq__item}>
          <div className={styles.faq__itemBody}>
             <div className={styles.faq__itemTitle}>{el}</div>
          </div>
